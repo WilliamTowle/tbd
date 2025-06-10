a0a12d3515da20e78d1e26bc12fcc1e1d052348b
William Towle <william_towle@yahoo.co.uk>
2025-06-10 22:50:37 +0100

    qdmusl-chroot1: create initrd.gz

--- config/qdmusl-chroot1/initrd.mk
+++ config/qdmusl-chroot1/initrd.mk
@@ -16,4 +16,5 @@ all-initrd: \
 	install-target-dash \
 	install-target-sbase \
 	install-target-ubase
+	( cd ${STAGING_DIR}/initrd && find . | cpio -o -H newc -R root:root | gzip -9 ) > ${STAGING_DIR}/initrd.gz
 	@printf '[initrd %s] %s\n' $@ "Done at `date +'%F, %X'`"
