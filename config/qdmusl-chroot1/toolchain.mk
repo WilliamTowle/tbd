172df372fe0760b4f0a1ad6ac7234f4289cb2179
William Towle <william_towle@yahoo.co.uk>
2025-06-10 21:22:20 +0100

    Add cross-binutils to qdmusl-chroot1 toolchain

--- config/qdmusl-chroot1/toolchain.mk
+++ config/qdmusl-chroot1/toolchain.mk
@@ -7,9 +7,11 @@ include ${CONFIG_DIR}/settings.mk
 PACKAGE_DESTINATION_RULES=toolchain
 
 include ${PACKAGE_DIR}/linux/v3.4.113.mk
+include ${PACKAGE_DIR}/cross-binutils/v2.23.2.mk
 
 all-toolchain: \
-	install-toolchain-lxheaders
+	install-toolchain-lxheaders \
+	install-toolchain-cross-binutils
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 distclean-toolchain:
