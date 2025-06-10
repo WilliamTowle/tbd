8af7e4720b14be6d8854c66c1dd5fbaf5f54a881
William Towle <william_towle@yahoo.co.uk>
2025-06-10 21:36:32 +0100

    Add musl to qdmusl-chroot1 toolchain

--- config/qdmusl-chroot1/toolchain.mk
+++ config/qdmusl-chroot1/toolchain.mk
@@ -9,11 +9,13 @@ PACKAGE_DESTINATION_RULES=toolchain
 include ${PACKAGE_DIR}/linux/v3.4.113.mk
 include ${PACKAGE_DIR}/cross-binutils/v2.23.2.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.7.3.mk
+include ${PACKAGE_DIR}/musl/v0.9.13.mk
 
 all-toolchain: \
 	install-toolchain-lxheaders \
 	install-toolchain-cross-binutils \
-	install-toolchain-cross-gcc
+	install-toolchain-cross-gcc \
+	install-toolchain-cross-musl
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 distclean-toolchain:
