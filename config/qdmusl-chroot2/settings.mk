a8e1de59df1edb639e145e0d556c9a87cf0c5e8f
William Towle <william_towle@yahoo.co.uk>
2025-06-10 22:13:29 +0100

    Add qdmusl-chroot2 toolchain/rootfs config

---
+++ config/qdmusl-chroot2/settings.mk
@@ -0,0 +1,22 @@
+## TBD -- "Tiny By Design" common configuration
+
+PACKAGE_DIR=${TOPLEV}/packages
+OUTPUT_DIR=${TOPLEV}
+DOWNLOAD_DIR=${OUTPUT_DIR}/downloads
+STAGING_DIR=${OUTPUT_DIR}/staging
+TOOLCHAIN_DIR=${CURDIR}/toolchain
+
+HOST_CPU=$(shell uname -m)
+#HOST_TRIPLET=$(shell gcc -dumpmachine | sed 's/^\([^-]*\).*-\([^-]*-[^-]*.\)$$/\1-unknown-\2/')
+HOST_TRIPLET=$(shell gcc -dumpmachine | sed 's/^\([^-]*\).*-\([^-]*-[^-]*.\)$$/\1-pc-\2/')
+
+#TARGET_CPU=i686
+TARGET_CPU=$(shell uname -m)
+TARGET_ARCH=$(shell echo ${TARGET_CPU} | sed 's/i[4-6]86/i386/' )
+#|TARGET_TRIPLET=${TARGET_CPU}-unknown-linux-musl
+TARGET_TRIPLET=${TARGET_CPU}-custom-linux-musl
+
+
+export PATH:=${TOOLCHAIN_DIR}/bin:${PATH}
+
+ALL_COMPONENTS=toolchain rootfs
