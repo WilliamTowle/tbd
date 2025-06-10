14432378fbb595f6ee50f9c085629f86c93b9710
William Towle <william_towle@yahoo.co.uk>
2025-06-10 22:05:53 +0100

    Add target dash to qdmusl-chroot1 initrd build

--- config/qdmusl-chroot1/initrd.mk
+++ config/qdmusl-chroot1/initrd.mk
@@ -7,7 +7,9 @@ include ${CONFIG_DIR}/settings.mk
 PACKAGE_DESTINATION_RULES=target
 PACKAGE_DESTDIR=${STAGING_DIR}/initrd
 include ${PACKAGE_DIR}/musl/v0.9.13.mk
+include ${PACKAGE_DIR}/dash/v0.5.12.mk
 
 all-initrd: \
-	install-target-musl
+	install-target-musl \
+	install-target-dash
 	@printf '[initrd %s] %s\n' $@ "Done at `date +'%F, %X'`"
