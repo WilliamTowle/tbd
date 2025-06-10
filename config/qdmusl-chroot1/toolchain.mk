60dd6d32c22a81823baa8f8bf6502b4204dd60ba
William Towle <william_towle@yahoo.co.uk>
2025-06-10 21:29:23 +0100

    Add cross-gcc to qdmusl-chroot1 toolchain

--- config/qdmusl-chroot1/toolchain.mk
+++ config/qdmusl-chroot1/toolchain.mk
@@ -8,10 +8,12 @@ PACKAGE_DESTINATION_RULES=toolchain
 
 include ${PACKAGE_DIR}/linux/v3.4.113.mk
 include ${PACKAGE_DIR}/cross-binutils/v2.23.2.mk
+include ${PACKAGE_DIR}/cross-gcc/v4.7.3.mk
 
 all-toolchain: \
 	install-toolchain-lxheaders \
-	install-toolchain-cross-binutils
+	install-toolchain-cross-binutils \
+	install-toolchain-cross-gcc
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 distclean-toolchain:
