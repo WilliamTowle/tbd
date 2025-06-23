e3af7ac2130736ad61302c0c89163fe8ba5edb56
William Towle <william_towle@yahoo.co.uk>
2025-06-23 23:53:53 +0100

    Add cross-kgcc to nbl toolchain build

--- config/nbl/toolchain.mk
+++ config/nbl/toolchain.mk
@@ -7,9 +7,11 @@ include ${CONFIG_DIR}/settings.mk
 PACKAGE_DESTINATION_RULES=toolchain
 
 include ${PACKAGE_DIR}/cross-binutils/v2.21.1.mk
+include ${PACKAGE_DIR}/cross-gcc/v4.3.6.mk
 
 all-toolchain: \
-	install-toolchain-cross-binutils
+	install-toolchain-cross-binutils \
+	install-toolchain-cross-kgcc
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-toolchain:
