e0af66e9cdd4072a3659f3525b1b3a13fe8ee610
William Towle <william_towle@yahoo.co.uk>
2025-06-24 08:38:42 +0100

    Add uClibc startfiles to nbl toolchain build

--- config/nbl/toolchain.mk
+++ config/nbl/toolchain.mk
@@ -9,11 +9,13 @@ PACKAGE_DESTINATION_RULES=toolchain
 include ${PACKAGE_DIR}/cross-binutils/v2.21.1.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.3.6.mk
 include ${PACKAGE_DIR}/linux/v3.4.113-nbl.mk
+include ${PACKAGE_DIR}/uClibc/v0.9.33.2.mk
 
 all-toolchain: \
 	install-toolchain-cross-binutils \
 	install-toolchain-cross-kgcc \
-	install-toolchain-lxheaders
+	install-toolchain-lxheaders \
+	install-cross-uclibc-startfiles
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-toolchain:
