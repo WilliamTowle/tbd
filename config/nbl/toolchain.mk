ec97ae586ab27661ee764a76ba61b4f92167e198
William Towle <william_towle@yahoo.co.uk>
2025-06-24 10:53:38 +0100

    Add uClibc libc to nbl toolchain build

--- config/nbl/toolchain.mk
+++ config/nbl/toolchain.mk
@@ -16,7 +16,8 @@ all-toolchain: \
 	install-toolchain-cross-kgcc \
 	install-toolchain-lxheaders \
 	install-cross-uclibc-startfiles \
-	install-cross-libgcc
+	install-cross-libgcc \
+	install-cross-uclibc-libc
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-toolchain:
