b718741be311720d52614465f42d64766971bbd5
William Towle <william_towle@yahoo.co.uk>
2025-07-04 09:49:41 +0100

    Package configuration for glibc libc

--- packages/glibc/v2.14.mk
+++ packages/glibc/v2.14.mk
@@ -91,4 +91,45 @@ install-cross-glibc-startfiles: build-cross-glibc-startfiles
 		install csu/crt1.o csu/crti.o csu/crtn.o ${TOOLCHAIN_DIR}/lib && \
 		${TARGET_TRIPLET}-kgcc -nostdlib -nostartfiles -shared -x c /dev/null -o ${TOOLCHAIN_DIR}/lib/libc.so ;\
 	}
+
+
+.PHONY: build-cross-glibc-libc
+
+build-cross-glibc-libc: prepare-cross-glibc
+	@printf '%s %s: %s\n' $(lastword ${MAKEFILE_LIST}) $@ "Reached at `date +'%X, %F'`"
+	mkdir -p ${CROSS_GLIBC_SRC_TREE}/$@
+	[ -r ${CROSS_GLIBC_SRC_TREE}/$@/config.status ] || { \
+		printf '[%s] %s\n' $@ 'Configure...' && \
+		cd ${CROSS_GLIBC_SRC_TREE}/$@ && \
+		CC=${TARGET_TRIPLET}-kgcc \
+		  AS=${TARGET_TRIPLET}-kas \
+		  LD=${TARGET_TRIPLET}-kld \
+		  libc_cv_forced_unwind=yes \
+		  libc_cv_c_cleanup=yes \
+			../configure \
+			  --prefix=/ \
+			  --includedir=/usr/include \
+				$(shell echo "--includedir=${TOOLCHAIN_DIR}/usr/include" >/dev/null) \
+			  --build=${HOST_TRIPLET} --host=${TARGET_TRIPLET} \
+			  --target=${TARGET_TRIPLET} \
+			  --with-headers=${TOOLCHAIN_DIR}/usr/include \
+			  --with-pkgversion='qdxtc' --enable-kernel=3.2.4 $(shell echo '--enable-kernel=2.6.28' >/dev/null) \
+			  --disable-profile --enable-add-ons \
+			  $(shell echo '--with-tls' >/dev/null) ;\
+	}
+	[ -r ${CROSS_GLIBC_SRC_TREE}/$@/csu/crtn.o ] || { \
+		printf '[%s] %s\n' $@ 'Build...' && \
+		cd ${CROSS_GLIBC_SRC_TREE}/$@ && \
+		make ;\
+	}
+
+
+.PHONY: install-cross-glibc-libc
+
+install-cross-glibc-libc: build-cross-glibc-libc
+	[ -r ${TOOLCHAIN_DIR}/lib/libc-${CROSS_GLIBC_VERSION}.so ] || { \
+		printf '[%s] %s\n' $@ 'Install...' && \
+		cd ${CROSS_GLIBC_SRC_TREE}/$(patsubst install-%,build-%,$@) && \
+		make install_root=${TOOLCHAIN_DIR} install ;\
+	}
 endif
