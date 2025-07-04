ea9a2e72ef27a8f30706c7b8ea5eb17a7c3fb753
William Towle <william_towle@yahoo.co.uk>
2025-07-04 17:21:41 +0100

    Add target dash to qdglibc rootfs build

--- config/qdglibc/rootfs.mk
+++ config/qdglibc/rootfs.mk
@@ -8,9 +8,11 @@ PACKAGE_DESTINATION_RULES=target
 PACKAGE_DESTDIR=${STAGING_DIR}/rootfs
 
 include ${PACKAGE_DIR}/glibc/v2.14.mk
+include ${PACKAGE_DIR}/dash/v0.5.8.mk
 
 all-rootfs: \
-	install-target-glibc
+	install-target-glibc \
+	install-target-dash
 	@printf '[rootfs %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-rootfs:
