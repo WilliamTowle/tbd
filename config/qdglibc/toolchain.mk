f81d798895232360afc40c2ccb0281be21d93577
William Towle <william_towle@yahoo.co.uk>
2025-07-04 09:05:48 +0100

    Add glibc startfiles to qdglibc toolchain build

--- config/qdglibc/toolchain.mk
+++ config/qdglibc/toolchain.mk
@@ -8,12 +8,14 @@ PACKAGE_DESTINATION_RULES=toolchain
 
 include ${PACKAGE_DIR}/cross-binutils/v2.21.1.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.4.7.mk
+include ${PACKAGE_DIR}/glibc/v2.14.mk
 include ${PACKAGE_DIR}/linux/v3.4.113-nbl.mk
 
 all-toolchain: \
 	install-toolchain-cross-binutils \
 	install-toolchain-cross-kgcc \
-	install-toolchain-lxheaders
+	install-toolchain-lxheaders \
+	install-cross-glibc-startfiles
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-toolchain:
