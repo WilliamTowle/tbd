72f22a614fd5bcb37646a2ceec9a7f57f6d451d0
William Towle <william_towle@yahoo.co.uk>
2025-07-04 09:10:48 +0100

    Add cross-libgcc to qdglibc toolchain build

--- config/qdglibc/toolchain.mk
+++ config/qdglibc/toolchain.mk
@@ -15,7 +15,8 @@ all-toolchain: \
 	install-toolchain-cross-binutils \
 	install-toolchain-cross-kgcc \
 	install-toolchain-lxheaders \
-	install-cross-glibc-startfiles
+	install-cross-glibc-startfiles \
+	install-cross-libgcc
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-toolchain:
