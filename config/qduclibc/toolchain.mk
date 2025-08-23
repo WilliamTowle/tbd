967475620bbbefe26d85126214870620def6336c
William Towle <william_towle@yahoo.co.uk>
2025-08-23 15:12:44 +0100

    Add kernel headers to qduclibc toolchain build

--- config/qduclibc/toolchain.mk
+++ config/qduclibc/toolchain.mk
@@ -7,9 +7,11 @@ include ${CONFIG_DIR}/settings.mk
 PACKAGE_DESTINATION_RULES=toolchain
 
 include ${PACKAGE_DIR}/cross-binutils/v2.21.1-qdsp.mk
+include ${PACKAGE_DIR}/linux/v3.4.113-nbl.mk
 
 all-toolchain: \
-	install-toolchain-cross-binutils
+	install-toolchain-cross-binutils \
+	install-toolchain-lxheaders
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-toolchain:
