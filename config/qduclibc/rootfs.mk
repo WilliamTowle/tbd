ec0c66f330692ef4ceae25a7f6beb35718a53821
William Towle <william_towle@yahoo.co.uk>
2025-08-23 18:02:03 +0100

    Add target busybox to qduclibc rootfs build

--- config/qduclibc/rootfs.mk
+++ config/qduclibc/rootfs.mk
@@ -7,10 +7,12 @@ include ${CONFIG_DIR}/settings.mk
 PACKAGE_DESTINATION_RULES=target
 PACKAGE_DESTDIR=${STAGING_DIR}/rootfs
 
+include ${PACKAGE_DIR}/busybox/v1.20.2.mk
 include ${PACKAGE_DIR}/uClibc-ng/v1.0.0.mk
 
 all-rootfs: \
-	install-target-uclibc
+	install-target-uclibc \
+	install-target-busybox
 	@printf '[rootfs %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 clean-rootfs:
