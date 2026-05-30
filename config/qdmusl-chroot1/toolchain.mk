8ed17838084b5938d97310e4d4749c03e26616eb
William Towle <william_towle@yahoo.co.uk>
2026-05-30 21:50:43 +0100

    qdmusl-chroot1: align includes
    Align toolchain and initrd include directive to suit the change to
    musl version.

--- config/qdmusl-chroot1/toolchain.mk
+++ config/qdmusl-chroot1/toolchain.mk
@@ -8,7 +8,7 @@
 include ${PACKAGE_DIR}/linux/v3.4.113.mk
 include ${PACKAGE_DIR}/cross-binutils/v2.23.2.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.7.3.mk
-include ${PACKAGE_DIR}/musl/v0.9.13.mk
+include ${PACKAGE_DIR}/musl/v0.9.15.mk
 
 #
 
