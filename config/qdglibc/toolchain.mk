3fca38fd257a596e0212cb16e99959fe7ba6afda
William Towle <william_towle@yahoo.co.uk>
2026-06-01 22:24:16 +0100

    qdglibc config: declare toolchain packages

--- config/qdglibc/toolchain.mk
+++ config/qdglibc/toolchain.mk
@@ -1,6 +1,18 @@
 ## TBD -- "Tiny By Design" toolchain component
 
-TOOLCHAIN_PACKAGES=
+include ${PACKAGE_DIR}/cross-binutils/v2.21.1-qdnbl.mk
+include ${PACKAGE_DIR}/cross-gcc/v4.4.7-qdnbl.mk
+include ${PACKAGE_DIR}/glibc/v2.14-qdglibc.mk
+include ${PACKAGE_DIR}/linux/v3.4.113-qdnbl.mk
+
+TOOLCHAIN_PACKAGES= \
+		toolchain-cross-binutils \
+		toolchain-cross-kgcc \
+		toolchain-lxheaders \
+		cross-glibc-startfiles \
+		cross-libgcc \
+		cross-glibc-libc \
+		cross-gcc
 
 ifneq (${TOOLCHAIN_PACKAGES},)
 include ${CONFIG_DIR}/functions.mk
