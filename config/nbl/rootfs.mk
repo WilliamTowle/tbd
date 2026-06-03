a8dfbd316ddf01c2e7b24a3094ffc561f488cc49
William Towle <william_towle@yahoo.co.uk>
2026-06-03 19:22:24 +0100

    nbl: Add {TOOLCHAIN|ROOTFS}_PACKAGES
    Introduce TOOLCHAIN_PACKAGES and ROOTFS_PACKAGES variables to nbl
    builds.
    
    This change aims to simplify checking that the included files and
    the rules triggered within them are aligned. Through use of these
    variables we can also report the number of packages built.

--- config/nbl/rootfs.mk
+++ config/nbl/rootfs.mk
@@ -2,10 +2,16 @@
 
 # packages
 
--include ${CONFIG_DIR}/functions.mk
-
 include ${PACKAGE_DIR}/busybox/v1.20.2.mk
 include ${PACKAGE_DIR}/uClibc/v0.9.33.2-qdnbl.mk
+
+ROOTFS_PACKAGES= \
+	target-uclibc \
+	target-busybox
+
+ifneq (${ROOTFS_PACKAGES},)
+-include ${CONFIG_DIR}/functions.mk
+endif
 
 
 #
@@ -16,9 +22,8 @@ PACKAGE_DESTDIR=${STAGING_DIR}/rootfs
 
 all-rootfs: \
 	all-toolchain \
-	install-target-uclibc \
-	install-target-busybox
-	@printf '[rootfs %s] %s\n' $@ "Done at `date +'%F, %X'`"
+	$(addprefix install-,${ROOTFS_PACKAGES})
+	@printf '[rootfs %s] %s\n' $@ "Done (built $(words ${ROOTFS_PACKAGES})) at `date +'%F, %X'`"
 
 ##
 
--- config/nbl/toolchain.mk
+++ config/nbl/toolchain.mk
@@ -3,12 +3,23 @@
 
 # packages
 
--include ${CONFIG_DIR}/functions.mk
-
 include ${PACKAGE_DIR}/cross-binutils/v2.21.1-qdnbl.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.3.6-qdnbl.mk
 include ${PACKAGE_DIR}/linux/v3.4.113-qdnbl.mk
 include ${PACKAGE_DIR}/uClibc/v0.9.33.2-qdnbl.mk
+
+TOOLCHAIN_PACKAGES= \
+	toolchain-cross-binutils \
+	toolchain-cross-kgcc \
+	toolchain-lxheaders \
+	cross-uclibc-startfiles \
+	cross-libgcc \
+	cross-uclibc-libc \
+	cross-gcc
+
+ifneq (${TOOLCHAIN_PACKAGES},)
+-include ${CONFIG_DIR}/functions.mk
+endif
 
 #
 
@@ -18,14 +29,9 @@ ${TOOLCHAIN_DIR}: ; @mkdir -p $@
 .PHONY: all-toolchain
 
 all-toolchain: \
-	install-toolchain-cross-binutils \
-	install-toolchain-cross-kgcc \
-	install-toolchain-lxheaders \
-	install-cross-uclibc-startfiles \
-	install-cross-libgcc \
-	install-cross-uclibc-libc \
-	install-cross-gcc
-	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
+	$(addprefix install-,${TOOLCHAIN_PACKAGES}) \
+	| ${TOOLCHAIN_DIR}
+	@printf '[toolchain %s] %s\n' $@ "Done (built $(words ${TOOLCHAIN_PACKAGES})) at `date +'%F, %X'`"
 
 #
 
