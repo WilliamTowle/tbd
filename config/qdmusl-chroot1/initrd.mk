67109519ac663523f41976ba70b185ba7d06fc17
William Towle <william_towle@yahoo.co.uk>
2026-06-03 19:26:46 +0100

    qdmusl-chroot1: Add {TOOLCHAIN|ROOTFS}_PACKAGES
    Introduce TOOLCHAIN_PACKAGES and ROOTFS_PACKAGES variables to
    qdmusl-chroot1 builds.
    
    This change aims to simplify checking that the included files and
    the rules triggered within them are aligned. Through use of these
    variables we can also report the number of packages built.

--- config/qdmusl-chroot1/initrd.mk
+++ config/qdmusl-chroot1/initrd.mk
@@ -3,19 +3,22 @@
 
 # packages
 
--include ${CONFIG_DIR}/functions.mk
-
-include ${PACKAGE_DIR}/musl/v0.9.15.mk
 include ${PACKAGE_DIR}/dash/v0.5.12.mk
+include ${PACKAGE_DIR}/musl/v0.9.15.mk
 include ${PACKAGE_DIR}/sbase/v0.1.mk
 include ${PACKAGE_DIR}/ubase/v0.1.mk
+
+INITRD_PACKAGES=musl dash sbase ubase
+
+ifneq (${INITRD_PACKAGES},)
+-include ${CONFIG_DIR}/functions.mk
+endif
 
 
 #
 
 INITRD_STAGING_DIR=${STAGING_DIR}/initrd
 INITRD_MEDIA_TYPE=cpio
-INITRD_PACKAGES=musl dash sbase ubase
 
 
 ${INITRD_STAGING_DIR}: ; mkdir -p $@
@@ -28,7 +31,7 @@ ${INITRD_STAGING_DIR}: ; mkdir -p $@
 
 prepare-initrd-staging: \
 		all-toolchain \
-		$(patsubst %,build-target-%,${INITRD_PACKAGES}) \
+		$(addprefix build-target-,${INITRD_PACKAGES}) \
 		| ${STAGING_DIR}
 
 
@@ -38,7 +41,7 @@ install-initrd-cpio: PACKAGE_DESTDIR=${INITRD_STAGING_DIR}
 
 install-initrd-cpio: \
 		prepare-initrd-staging \
-		$(patsubst %,install-target-%,${INITRD_PACKAGES}) \
+		$(addprefix install-target-,${INITRD_PACKAGES}) \
 		| ${INITRD_STAGING_DIR}
 	( cd ${INITRD_STAGING_DIR} && find . | cpio -o -H newc -R root:root | gzip -9 ) > ${INITRD_STAGING_DIR}.gz
 
@@ -49,13 +52,13 @@ install-initrd-squashfs: PACKAGE_DESTDIR=${INITRD_STAGING_DIR}
 
 install-initrd-squashfs: \
 		prepare-initrd-staging \
-		$(patsubst %,install-target-%,${INITRD_PACKAGES}) \
+		$(addprefix install-target-,${INITRD_PACKAGES}) \
 		| ${INITRD_STAGING_DIR}
 	( cd ${INITRD_STAGING_DIR} && /usr/bin/mksquashfs . ${INITRD_STAGING_DIR}.squashfs -noappend -all-root -nopad )
 
 
 all-initrd: install-initrd-${INITRD_MEDIA_TYPE}
-	@printf '[initrd %s] %s\n' $@ "Done at `date +'%F, %X'`"
+	@printf '[initrd %s] %s\n' $@ "Done (built $(words ${INITRD_PACKAGES})) at `date +'%F, %X'`"
 
 #
 
--- config/qdmusl-chroot1/toolchain.mk
+++ config/qdmusl-chroot1/toolchain.mk
@@ -3,12 +3,21 @@
 
 # packages
 
--include ${CONFIG_DIR}/functions.mk
-
-include ${PACKAGE_DIR}/linux/v3.4.113.mk
 include ${PACKAGE_DIR}/cross-binutils/v2.23.2.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.7.3.mk
+include ${PACKAGE_DIR}/linux/v3.4.113.mk
 include ${PACKAGE_DIR}/musl/v0.9.15.mk
+
+TOOLCHAIN_PACKAGES= \
+	toolchain-lxheaders \
+	toolchain-cross-binutils \
+	toolchain-cross-gcc \
+	toolchain-cross-musl
+
+ifneq (${TOOLCHAIN_PACKAGES},)
+-include ${CONFIG_DIR}/functions.mk
+endif
+
 
 #
 
@@ -18,10 +27,7 @@ ${TOOLCHAIN_DIR}: ; @mkdir -p $@
 .PHONY: all-toolchain
 
 all-toolchain: \
-	install-toolchain-lxheaders \
-	install-toolchain-cross-binutils \
-	install-toolchain-cross-gcc \
-	install-toolchain-cross-musl
+	$(addprefix install-,${TOOLCHAIN_PACKAGES})
 	@printf '[toolchain %s] %s\n' $@ "Done at `date +'%F, %X'`"
 
 #
