3bb54a952a39dc665dc04894911509a2c96de33d
William Towle <william_towle@yahoo.co.uk>
2026-08-09 22:16:34 +0100

    qdnbl config: rework target deployment
    Introduce TARGET_DEPLOY_TYPE to control whether the final install
    step populates a directory (TARGET_DEPLOY_TYPE=fstree) or creates
    archives (TARGET_DEPLOY_TYPE=tarballs).
    
    Rules and variables for the output directories in each case (ie.
    TARGET_PACKAGES_DIR, TARGET_STAGING_DIR) get renamed and moved
    alongside.

--- config/qdnbl/rootfs.mk
+++ config/qdnbl/rootfs.mk
@@ -4,45 +4,38 @@ include ${PACKAGE_DIR}/busybox/v1.20.2.mk
 include ${PACKAGE_DIR}/uClibc/v0.9.33.2-qdnbl.mk
 
 ROOTFS_PACKAGES=busybox uclibc
-ROOTFS_PACKAGES_DIR=${STAGING_DIR}/rootfs-packages
-ROOTFS_STAGING_DIR=${STAGING_DIR}/rootfs
 
 ifneq (${ROOTFS_PACKAGES},)
 include ${CONFIG_DIR}/functions.mk
 endif
 
 
-##
-
-${ROOTFS_PACKAGES_DIR}: ; @mkdir -p $@
-${ROOTFS_STAGING_DIR}: ; @mkdir -p $@
-
 
 ## prepare phase
 
-.PHONY: rootfs-prepare-build rootsfs-prepare-fstree rootfs-prepare-tarballs
+.PHONY: rootfs-prepare-build rootfs-prepare-fstree rootfs-prepare-tarballs
 
 rootfs-prepare-build: all-toolchain
 
 rootfs-prepare-fstree: \
 		rootfs-prepare-build \
-		| ${ROOTFS_STAGING_DIR}
+		| ${TARGET_STAGING_DIR}
 
 rootfs-prepare-tarballs: \
 		rootfs-prepare-build \
-		| ${ROOTFS_PACKAGES_DIR}
+		| ${TARGET_PACKAGES_DIR}
 
 
 ##
 
 .PHONY: rootfs-deploy-fstree
 
-rootfs-deploy-fstree: FSTREE_DESTDIR=${ROOTFS_STAGING_DIR}
+rootfs-deploy-fstree: FSTREE_DESTDIR=${TARGET_STAGING_DIR}
 
 rootfs-deploy-fstree: \
 		rootfs-prepare-build \
 		$(patsubst %,build-target-%,${ROOTFS_PACKAGES}) \
-		| ${ROOTFS_STAGING_DIR}
+		| ${TARGET_STAGING_DIR}
 ifneq (${ROOTFS_PACKAGES},)
 	for PKG in ${ROOTFS_PACKAGES} ; do \
 		${MAKE} -f $(firstword ${MAKEFILE_LIST}) install-target-$${PKG} PACKAGE_DESTDIR=${FSTREE_DESTDIR} || exit 1 ;\
@@ -54,21 +47,20 @@ endif
 
 .PHONY: $(patsubst %,rootfs-build-tarball-%,${ROOTFS_PACKAGES})
 
-$(patsubst %,rootfs-build-tarball-%,${ROOTFS_PACKAGES}): TARBALL_DESTDIR=${ROOTFS_PACKAGES_DIR}/$*
+$(patsubst %,rootfs-build-tarball-%,${ROOTFS_PACKAGES}): TARBALL_DESTDIR=${TARGET_PACKAGES_DIR}/$*
 
 rootfs-build-tarball-busybox: PKGVER=${TARGET_BUSYBOX_VERSION}
 rootfs-build-tarball-glibc: PKGVER=${COMMON_GLIBC_VERSION}
-rootfs-build-tarball-bootfiles-kernel: PKGVER=${TARGET_LINUX_VERSION}
 
 
 $(patsubst %,rootfs-build-tarball-%,${ROOTFS_PACKAGES}): rootfs-build-tarball-%: \
-		| ${ROOTFS_PACKAGES_DIR}
-	mkdir ${ROOTFS_PACKAGES_DIR}/$*
+		| ${TARGET_PACKAGES_DIR}
+	mkdir ${TARGET_PACKAGES_DIR}/$*
 	${MAKE} -f $(firstword ${MAKEFILE_LIST}) install-target-$* PACKAGE_DESTDIR=${TARBALL_DESTDIR}
 	( cd ${TARBALL_DESTDIR} &&\
 		tar --create --posix --owner=root --group=root * ) \
-			| ( cd ${ROOTFS_PACKAGES_DIR}/ && gzip -9 > $*-$(if ${PKGVER},${PKGVER},UNKNOWN).tgz )
-	rm -rf ${ROOTFS_PACKAGES_DIR}/$*
+			| ( cd ${TARGET_PACKAGES_DIR}/ && gzip -9 > $*-$(if ${PKGVER},${PKGVER},UNKNOWN).tgz )
+	rm -rf ${TARGET_PACKAGES_DIR}/$*
 
 
 .PHONY: rootfs-deploy-tarballs
@@ -80,15 +72,14 @@ rootfs-deploy-tarballs: \
 
 .PHONY: rootfs-deploy
 
-rootfs-deploy: rootfs-deploy-fstree
-#rootfs-deploy: rootfs-deploy-tarballs
+rootfs-deploy: rootfs-deploy-${TARGET_DEPLOY_TYPE}
 
 
 ##
 
 .PHONY: clean-rootfs
 clean-rootfs:
-	-rm -rf ${ROOTFS_STAGING_DIR}
+	-rm -rf ${TARGET_STAGING_DIR}
 
 #
 
