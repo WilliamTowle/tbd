2ed25c12acee055c6cf2b33887dca4fcd07e64c2
William Towle <william_towle@yahoo.co.uk>
2026-08-17 09:03:24 +0100

    qdnbl config: adjust kernel version

--- config/qdnbl/toolchain.mk
+++ config/qdnbl/toolchain.mk
@@ -2,7 +2,7 @@
 
 include ${PACKAGE_DIR}/cross-binutils/v2.21.1-qdnbl.mk
 include ${PACKAGE_DIR}/cross-gcc/v4.3.6-qdnbl.mk
-include ${PACKAGE_DIR}/linux/v3.4.113-qdnbl.mk
+include ${PACKAGE_DIR}/linux/v3.18.109-qdnbl.mk
 include ${PACKAGE_DIR}/uClibc/v0.9.33.2-qdnbl.mk
 
 TOOLCHAIN_PACKAGES= \
